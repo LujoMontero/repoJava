6eed48eaee154743901ede24a5d8c27e0b585bc9
luis <luisjosemon@gmail.com>
2024-04-16 20:31:40 -0400

    agregando fecha

--- src/desafio1/fechas.java
+++ src/desafio1/fechas.java
@@ -6,5 +6,14 @@ public class fechas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Ingrese la primera fecha como DD/MM/AAAA");
+        String fecha = sc.nextLine();
+
+        System.out.println("Ingrese la segunda fecha como MM/AAAA");
+        String fecha2 = sc.nextLine();
+
+        System.out.println("fecha = " + fecha);
+        System.out.println("fecha2 = " + fecha2);
+
     }
 }
