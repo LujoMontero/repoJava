65bbbd9ed01a93deb93c5fb31c6d52db935a83e0
luis <luisjosemon@gmail.com>
2024-04-18 17:21:39 -0400

    nuevo desafio

---
+++ src/desafioOpcional2/WhileDowhile.java
@@ -0,0 +1,9 @@
+package desafioOpcional2;
+
+import java.util.Scanner;
+
+public class WhileDowhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
