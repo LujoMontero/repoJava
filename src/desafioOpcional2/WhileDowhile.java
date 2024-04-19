0f3bc8f3c537ff9b2bf42aee73d6fd5a00fd825b
luis <luisjosemon@gmail.com>
2024-04-19 00:17:03 -0400

    ejercicio 2 conteo de pares

--- src/desafioOpcional2/WhileDowhile.java
+++ src/desafioOpcional2/WhileDowhile.java
@@ -6,18 +6,45 @@ public class WhileDowhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int i = 0;
-        while (i<50) {
-            i+=1;
-            System.out.printf("Iteración %d\n", i);
+//        int i = 0;
+//        while (i<50) {
+//            i+=1;
+//            System.out.printf("Iteración %d\n", i);
+//        }
+//        //Convertir el código anterior utilizando la sentencia do while
+//        //ejercicio 1
+//        int i2 = 0;
+//        do {
+//                i2+=1;
+//                System.out.printf("Iteración %d\n", i2);
+//        } while (i2<50);
+
+        //Ejercicio - Solo pares
+
+        System.out.println("limite de pares:");
+        int limite = sc.nextInt();
+        int contador = 0;
+            for (int i=0;contador<limite;i++){
+
+                if (i%2==0){
+                    System.out.println(i);
+                    contador++;
+                }
+
+            }
+
+        System.out.println("-----------------------");
+
+        int contador2 = 0;
+        for (int i=1;contador2<limite;i++){
+
+            if (i%2==0){
+                System.out.println(i);
+                contador2++;
+            }
+
         }
-        //Convertir el código anterior utilizando la sentencia do while
-        //ejercicio 1
-        int i2 = 0;
-        do {
-                i2+=1;
-                System.out.printf("Iteración %d\n", i2);
-        } while (i2<50);
+
 
 
 
