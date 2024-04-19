2e6393182bcbe1febf2e770fa36bee03fe3a7ec5
luis <luisjosemon@gmail.com>
2024-04-18 23:47:17 -0400

    ejercicio do while

--- src/desafioOpcional2/WhileDowhile.java
+++ src/desafioOpcional2/WhileDowhile.java
@@ -5,5 +5,22 @@ import java.util.Scanner;
 public class WhileDowhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int i = 0;
+        while (i<50) {
+            i+=1;
+            System.out.printf("Iteración %d\n", i);
+        }
+        //Convertir el código anterior utilizando la sentencia do while
+        //ejercicio 1
+        int i2 = 0;
+        do {
+                i2+=1;
+                System.out.printf("Iteración %d\n", i2);
+        } while (i2<50);
+
+
+
+
     }
 }
