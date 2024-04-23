efaddaf6b53d638a93d225d6550f7d2fb274288e
luis <luisjosemon@gmail.com>
2024-04-23 13:04:47 -0400

    desafio patrones

--- src/desafio2/patrones.java
+++ src/desafio2/patrones.java
@@ -1,4 +1,10 @@
 package desafio2;
 
+import java.util.Scanner;
+
 public class patrones {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese ????");
+    }
 }
