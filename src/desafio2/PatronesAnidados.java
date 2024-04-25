60272169e3a4057e606dade0ad3ea05bb463831f
luis <luisjosemon@gmail.com>
2024-04-25 10:08:30 -0400

    ejercicio dos con metodos

--- src/desafio2/PatronesAnidados.java
+++ src/desafio2/PatronesAnidados.java
@@ -16,27 +16,68 @@ public class PatronesAnidados {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //Patrón 1
+        // Ejercicio 2 - Patrones anidados
 
-        //Patrón 2
+        String opcion;
+        do {
+            menu();
+            opcion = interacionValidacion();
 
-        //Patrón 3
+            switch (opcion) {
+                case "1":
+                    // Patrón 1
+                    System.out.printf(ANSI_YELLOW + "Ingresa un numero: ");
+                    int n = sc.nextInt();
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < n; j++) {
+                            if ((i == 0 || i == n - 1) || (j == 0 || j == n - 1)) {
+                                System.out.print(ANSI_RED + "*");
+                            } else {
+                                System.out.print(" ");
+                            }
+                        }
+                        System.out.println();
+                    }
+                    break;
 
-        //Patrón 4
+                case "2":
+                    // Patrón 2
 
+                    break;
 
-        System.out.printf(ANSI_YELLOW + "Ingresa un numero: ");
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((i == 0 || i == n - 1) || (j == 0 || j == n - 1)) {
-                    System.out.print(ANSI_RED + "*");
-                } else {
-                    System.out.print(" ");
-                }
+                case "3":
+                    // Patrón 3
+
+                    break;
+
+                case "0":
+                    System.out.println("Saliendo del programa...doble enter para finalizar");
+                    break;
             }
-            System.out.println();
+            sc.nextLine(); // Limpia el buffer del scanner
+
+        } while (!opcion.equals("0"));
+    }
+
+    static public void menu() {
+        System.out.println(ANSI_YELLOW + "Selecciona el patrón que deseas mostrar:");
+        System.out.println(ANSI_YELLOW + "1. Patrón 1");
+        System.out.println(ANSI_BLUE + "2. Patrón 2");
+        System.out.println(ANSI_BLUE + "3. Patrón 3");
+        System.out.println(ANSI_RED + "0. Salir");
+        System.out.println(ANSI_RED + "Ingrese su opción: ");
+    }
+
+    static public String interacionValidacion() {
+        Scanner sc = new Scanner(System.in);
+        String opcion = "";
+        opcion = sc.nextLine();
+
+        while (!(opcion.equals("1") || opcion.equals("2")) && (!(opcion.equals("3") || opcion.equals("4"))) && (!opcion.equals("0"))) {
+            System.out.println("no es una opcion");
+            System.out.println("vuelva ingresar una opcion valida");
+            opcion = sc.nextLine();
         }
+        return opcion;
     }
 }
-
--- src/desafio2/patrones.java
+++ src/desafio2/patrones.java
@@ -30,66 +30,66 @@ public class patrones {
 
             opcion = sc.nextLine();
 
-                while (!(opcion.equals("1") || opcion.equals("2")) && (!(opcion.equals("3") || opcion.equals("4"))) && (!opcion.equals("0"))) {
-                    System.out.println("no es una opcion");
-                    System.out.println("vuelva ingresar una opcion valida");
-                    opcion = sc.nextLine();
-                }
+            while (!(opcion.equals("1") || opcion.equals("2")) && (!(opcion.equals("3") || opcion.equals("4"))) && (!opcion.equals("0"))) {
+                System.out.println("no es una opcion");
+                System.out.println("vuelva ingresar una opcion valida");
+                opcion = sc.nextLine();
+            }
 
-                switch (opcion) {
-                    case "1":
-                        // Patrón 1
-                        System.out.println(ANSI_YELLOW + "Ingresa un numero para el Patrón 1: ");
-                        int numero1 = sc.nextInt();
-                        for (int i = 0; i < numero1; i++) {
-                            if (i % 2 == 0) {
-                                System.out.print(ANSI_GREEN + "*");
+            switch (opcion) {
+                case "1":
+                    // Patrón 1
+                    System.out.println(ANSI_YELLOW + "Ingresa un numero para el Patrón 1: ");
+                    int numero1 = sc.nextInt();
+                    for (int i = 0; i < numero1; i++) {
+                        if (i % 2 == 0) {
+                            System.out.print(ANSI_GREEN + "*");
+                        } else {
+                            System.out.print(ANSI_RED + ".");
+                        }
+                    }
+                    System.out.println(ANSI_RESET);
+                    System.out.println();
+                    break;
+
+                case "2":
+                    // Patrón 2
+                    System.out.println(ANSI_YELLOW + "Hasta qué número quiere que se repita el Patrón 2: ");
+                    int n2 = sc.nextInt();
+                    System.out.println(ANSI_YELLOW + "¿Cuántas veces quiere que se repita la secuencia?: ");
+                    int rep2 = sc.nextInt();
+                    int color2 = rep2 / 2;
+                    for (int i = 0; i < rep2; i++) {
+                        for (int j = 1; j <= n2; j++) {
+                            if (i < color2) {
+                                System.out.printf(ANSI_BLUE + "%d", j);
                             } else {
-                                System.out.print(ANSI_RED + ".");
+                                System.out.printf(ANSI_RED + "%d", j);
                             }
+
                         }
-                        System.out.println(ANSI_RESET);
-                        System.out.println();
-                        break;
+                    }
+                    System.out.println(ANSI_RESET);
+                    break;
 
-                    case "2":
-                        // Patrón 2
-                        System.out.println(ANSI_YELLOW + "Hasta qué número quiere que se repita el Patrón 2: ");
-                        int n2 = sc.nextInt();
-                        System.out.println(ANSI_YELLOW + "¿Cuántas veces quiere que se repita la secuencia?: ");
-                        int rep2 = sc.nextInt();
-                        int color2 = rep2 / 2;
-                        for (int i = 0; i < rep2; i++) {
-                            for (int j = 1; j <= n2; j++) {
-                                if (i < color2) {
-                                    System.out.printf(ANSI_BLUE + "%d", j);
-                                } else {
-                                    System.out.printf(ANSI_RED + "%d", j);
-                                }
-
-                            }
+                case "3":
+                    // Patrón 3
+                    System.out.println(ANSI_YELLOW + "Ingrese un numero para el Patrón 3: ");
+                    int numero3 = sc.nextInt();
+                    for (int i = 0; i <= numero3; i++) {
+                        if (i % 2 == 0) {
+                            System.out.print(ANSI_BLUE + "||");
+                        } else {
+                            System.out.print(ANSI_RED + "*");
                         }
-                        System.out.println(ANSI_RESET);
-                        break;
+                    }
+                    System.out.println(ANSI_RESET); // Restablecer color después del patrón
+                    break;
 
-                    case "3":
-                        // Patrón 3
-                        System.out.println(ANSI_YELLOW + "Ingrese un numero para el Patrón 3: ");
-                        int numero3 = sc.nextInt();
-                        for (int i = 0; i <= numero3; i++) {
-                            if (i % 2 == 0) {
-                                System.out.print(ANSI_BLUE + "||");
-                            } else {
-                                System.out.print(ANSI_RED + "*");
-                            }
-                        }
-                        System.out.println(ANSI_RESET); // Restablecer color después del patrón
-                        break;
-
-                    case "0":
-                        System.out.println("Saliendo del programa...doble enter para finalizar");
-                        break;
-                }
+                case "0":
+                    System.out.println("Saliendo del programa...doble enter para finalizar");
+                    break;
+            }
             sc.nextLine(); // Limpia el buffer del scanner
 
         } while (!opcion.equals("0"));
