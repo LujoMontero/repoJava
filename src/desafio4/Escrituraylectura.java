611aeaa50dc19acf71f194e208c34f92a43c8fc4
luis <luisjosemon@gmail.com>
2024-05-03 19:57:07 -0400

    desafio 4

---
+++ src/desafio4/Escrituraylectura.java
@@ -0,0 +1,10 @@
+package desafio4;
+
+import java.util.Scanner;
+
+public class Escrituraylectura {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
