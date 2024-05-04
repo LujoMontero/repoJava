041457a2154564308038e0f61627bcc1211af29b
luis <luisjosemon@gmail.com>
2024-05-03 21:16:26 -0400

    comenzando desafio

--- src/desafio4/Escrituraylectura.java
+++ src/desafio4/Escrituraylectura.java
@@ -1,10 +1,44 @@
 package desafio4;
 
-import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
 
 public class Escrituraylectura {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
 
+    public static void main(String[] args) throws IOException {
+        //es el metodo creado createDirectory();
+        createDirectory("diretory");
+        createArchivos("diretory/texto.txt");
+    }
+
+    public static void createDirectory(String dir) throws IOException {
+        //crea el directorio
+        File directory = new File("src/"+dir);
+        if (!directory.exists()) {
+            directory.mkdir();
+            System.out.println("File created successfully");
+
+        }else{
+            System.out.println("Directory already exists");
+        }
+    }
+    public static void createArchivos(String archivo) throws IOException {
+        File file = new File("src/" + archivo);
+        if (!file.exists()) {
+            file.createNewFile();
+            System.out.println("Archivo created successfully");
+        } else {
+            System.out.println("Archivo already exists");
+        }
     }
 }
