04646ef60112096b171958a3d7e2a13271d6b5fc
luis <luisjosemon@gmail.com>
2024-05-15 11:06:40 -0400

    automotora Main

---
+++ src/desafioOpAutomotora/MainAutomotora.java
@@ -0,0 +1,9 @@
+package desafioOpAutomotora;
+
+public class MainAutomotora {
+    public static void main(String[] args) {
+        Tienda tienda = new Tienda();
+        tienda.existeStock();
+
+    }
+}
